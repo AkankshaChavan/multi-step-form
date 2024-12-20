f94e8e397439d949ea784a601732f8df1c172364
Akanksha Chavan <akankshachavan54356@gmail.com>
2024-12-20 17:40:09 +0530

    landing page UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavComponent } from './pages/side-nav/side-nav.component';
+import { YourInfoComponent } from './pages/form/your-info/your-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideNavComponent],
+  imports: [RouterOutlet, SideNavComponent,
+    YourInfoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
