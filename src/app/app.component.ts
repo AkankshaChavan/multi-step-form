bb62e68a6ead5c5a34dd57cf183773815455a645
AkankshaChavan <94160825+AkankshaChavan@users.noreply.github.com>
2024-12-22 21:23:31 +0530

    sunday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavComponent } from './pages/side-nav/side-nav.component';
 import { YourInfoComponent } from './pages/form/your-info/your-info.component';
+import { SelectPlanComponent } from './pages/form/select-plan/select-plan.component';
+import { AddOnsComponent } from './pages/form/add-ons/add-ons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, SideNavComponent,
-    YourInfoComponent
+    YourInfoComponent, SelectPlanComponent,
+    AddOnsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
