058cea22984dcbf09c7d8935587a7a3d57ec9a4c
AkankshaChavan <94160825+AkankshaChavan@users.noreply.github.com>
2024-12-22 22:42:49 +0530

    UI part done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,16 @@ import { SideNavComponent } from './pages/side-nav/side-nav.component';
 import { YourInfoComponent } from './pages/form/your-info/your-info.component';
 import { SelectPlanComponent } from './pages/form/select-plan/select-plan.component';
 import { AddOnsComponent } from './pages/form/add-ons/add-ons.component';
+import { SummaryComponent } from './pages/form/summary/summary.component';
+import { ThankYouComponent } from './pages/form/thank-you/thank-you.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, SideNavComponent,
     YourInfoComponent, SelectPlanComponent,
-    AddOnsComponent
+    AddOnsComponent, SummaryComponent,
+    ThankYouComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
